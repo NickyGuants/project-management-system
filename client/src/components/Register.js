d01f1917ba514fc55457e65bb5a7ab6cbd4dc08f
NickyGuants <nicholasguantai528@gmail.com>
2021-11-19 16:12:56 +0300

    create register form

---
+++ client/src/components/Register.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Register = () => {
+    return (
+        <div className="form">
+            <h1>Sign Up</h1>
+            <form action="">
+                <div className="child">
+                    <label htmlFor="">Email Adderess</label>
+                    <input type="email" placeholder="Enter your email address" />
+                </div>
+                <div className="child">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" placeholder="Enter your name" />
+                </div>
+
+                <div className="child">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" placeholder="Enter your username" />
+                </div>
+                
+                <div className="child">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder="Enter your password" />
+                </div>
+
+                <div className="child">
+                    <label htmlFor="password">Confirm Password</label>
+                    <input type="password" placeholder="confirm your password" />
+                </div>
+                <div>
+                    <button>Sign Up</button>
+                </div>
+                
+                <div>
+                    <p>Already have an Account? <Link to="/login">Login</Link></p>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Register
