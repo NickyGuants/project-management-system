187b313f6c6033638a1627247aa2e46bbdb08661
NickyGuants <nicholasguantai528@gmail.com>
2021-11-21 14:01:00 +0300

    create the home component

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome Home</h1>
+            <p></p>
+        </div>
+    )
+}
+
+export default Home
