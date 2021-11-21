e1dd3608bd8ea0deeb69340816c7ec61f632a55a
NickyGuants <nicholasguantai528@gmail.com>
2021-11-21 13:59:12 +0300

    redux:dispatch register action in register compone

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,36 +1,90 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router'
+import {register} from '../redux/actions/userActions'
 
 const Register = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [username, setUsername] = useState('')
+    const [message, setMessage] = useState(null)
+    
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const userRegister = useSelector((state) => state.userRegister)
+    const { error, userInfo } = userRegister
+
+    useEffect(() => {
+        if (userInfo) {
+        navigate('/')
+        }
+    }, [navigate, userInfo])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (password !== confirmPassword) {
+          setMessage('Passwords do not match')
+        } else {
+          dispatch(register(username,name, email, password))
+        }
+      }
+
     return (
         <div className="form">
             <h1>Sign Up</h1>
-            <form action="">
+            {message && <h1>{ message }</h1>}
+            {error && <h1>{ error.message }</h1>}
+            <form onSubmit={handleSubmit}>
                 <div className="child">
-                    <label htmlFor="">Email Address</label>
-                    <input type="email" placeholder="Enter your email address" />
-                </div>
-                <div className="child">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" placeholder="Enter your name" />
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        placeholder="Enter your username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}  />
                 </div>
 
                 <div className="child">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" placeholder="Enter your username" />
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        placeholder="Enter your name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)} />
+                </div>
+
+                <div className="child">
+                    <label htmlFor="">Email Address</label>
+                    <input
+                        type="email"
+                        placeholder="Enter your email address"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}/>
                 </div>
                 
                 <div className="child">
                     <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Enter your password" />
+                    <input
+                        type="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}/>
                 </div>
 
                 <div className="child">
                     <label htmlFor="password">Confirm Password</label>
-                    <input type="password" placeholder="confirm your password" />
+                    <input
+                        type="password"
+                        placeholder="confirm your password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}/>
                 </div>
                 <div>
-                    <button>Submit</button>
+                    <button type="submit">Submit</button>
                 </div>
                 
                 <div>
