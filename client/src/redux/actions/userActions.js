848b6d1486facebb2fea31a265c491c3a14d1dfd
NickyGuants <nicholasguantai528@gmail.com>
2021-11-21 13:57:30 +0300

    redux: use axios to call the backend

---
+++ client/src/redux/actions/userActions.js
@@ -0,0 +1,54 @@
+import { Login, Register } from "../types";
+import axios from 'axios'
+
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: Login.Request
+        })
+
+        const config = {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }
+
+        const data = await axios.post('users/login', { email, password }, config)
+        
+        dispatch({
+            type: Login.Success,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: Login.Fail,
+            payload: error.response && error.response.data.messsage ? error.response.data.messsage: error.messsage
+        })
+    }
+}
+
+export const register = ( username, name, email, password ) => async (dispatch) => {
+    try {
+        dispatch({
+            type: Register.Request
+        })
+
+        const config = {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }
+
+        const data = await axios.post('users/signup', {  username, name, email, password }, config)
+        
+        dispatch({
+            type: Register.Success,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: Register.Fail,
+            payload: error.response && error.response.data.messsage ? error.response.data.messsage: error.messsage
+        })
+    }
+}
