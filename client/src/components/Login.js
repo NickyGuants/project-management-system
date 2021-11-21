e8d0f5c139c51d7a8f34bb9747320b2238b5e46d
NickyGuants <nicholasguantai528@gmail.com>
2021-11-21 13:58:30 +0300

    redux: dispatch login action in login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,23 +1,51 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../redux/actions/userActions'
+import { useNavigate } from 'react-router'
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const dispatch = useDispatch();
+    //const location = useLocation();
+    const navigate = useNavigate();
+
+    const userLogin = useSelector(state => state.userLogin);
+    const { loading, error, userInfo } = userLogin;
+
+    useEffect(() => {
+        if (userInfo) {
+            navigate('/')
+        }
+    }, [navigate, userInfo])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //dispatch the login action
+        dispatch(login(email, password))
+    }
+
+
     return (
         <div className="form">
             <h1>Sign In</h1>
-            <form action="">
+            {error && <h1>{error}</h1>}
+            {loading && <h1>Loading</h1>}
+            <form onSubmit={handleSubmit}>
                 <div className="child">
                     <label htmlFor="email">Email Address</label>
-                    <input type="email" placeholder="Enter your email" />
+                    <input type="email" placeholder="Enter your email" value={ email} onChange={(e) =>setEmail(e.target.value)} />
                 </div>
 
                 <div className="child">
                     <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Enter your password" />
+                    <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
 
                 <div>
-                    <button>Sign In</button>
+                    <button type="submit">Sign In</button>
                 </div>
 
                 <div>
@@ -28,4 +56,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default Login;
