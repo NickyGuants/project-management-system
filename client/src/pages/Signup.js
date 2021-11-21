1dc08fb7df2e7f24c8a672eebebbd6c8b0450a5f
NickyGuants <nicholasguantai528@gmail.com>
2021-11-21 14:01:29 +0300

    create the register page

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Register from '../components/Register'
+
+const Signup = () => {
+    return (
+        <div>
+            <Register />
+        </div>
+    )
+}
+
+export default Signup
