f7edeb05e360d092144f6eafd9952899e7fcadce
NickyGuants <nicholasguantai528@gmail.com>
2021-11-22 17:04:45 +0300

    fix: add error handling

--- client/src/redux/reducers/userReducers.js
+++ client/src/redux/reducers/userReducers.js
@@ -1,38 +1,40 @@
 import { Login, Register } from '../types'
 
-export const userLoginReducer = (state = {}, {type, payload }) => {
-    switch (type) {
+export const userLoginReducer = (state = {}, action) => {
+    switch (action.type) {
         case Login.Request:
             return { loading: true }
         
         case Login.Success:
             return {
                 loading: false,
-                userInfo: payload
+                userInfo: action.payload
             }
         case Login.Fail:
             return {
                 loading: false,
-                error: payload
+                error: action.payload
             }
         default:
             return state
     }
 }
 
-export const userRegisterReducer = (state = {}, { type, payload }) => {
-    switch (type) {
+export const userRegisterReducer = (state = {}, action) => {
+    switch (action.type) {
         case Register.Request:
-            return { loading: true }
+            return {
+                loading: true
+            }
         case Register.Success:
             return {
                 loading: false,
-                userInfo: payload
+                userInfo: action.payload
             }
         case Register.Fail:
             return {
                 loading: false,
-                error: payload
+                error: action.payload
             }
         default:
             return state
