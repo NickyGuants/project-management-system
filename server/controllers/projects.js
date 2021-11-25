10034ba25af72e3fe9e3d36f92bb06f329c9ae03
NickyGuants <nicholasguantai528@gmail.com>
2021-11-25 13:42:51 +0300

    delete project

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -36,7 +36,7 @@ exports.getSingleProject = async (req, res) => {
                 return res.status(400).send({message: "Oh, sorry, we appear to have a server error."});
             }
             if (results.recordset.length === 0) {
-                return res.status(406).send("No project with that id in the database.");
+                return res.status(406).send({ message: "No project with that id in the database." });
             }
             return res.status(201).send(results.recordset[0]);
         })
@@ -77,10 +77,32 @@ exports.updateProject = async (req, res) => {
                 console.log(err)
                 return res.status(500).send({message: "Oh, sorry, we appear to have a server error."})
             }
-            res.status(201).send("project details updated successfully");
+            res.status(201).send({ message: "project details updated successfully" });
         })
         
     } catch (error) {
         res.send(500).send(error.message)
+    }
+}
+
+exports.deleteProject = async (req, res) => {
+    try {
+        let id = parseInt(req.params.id)
+        let deleteProject = `delete from projects where project_id=${id}`
+        let pool = await sql.connect(config);
+        pool.request().query(deleteProject, (err, results) => {
+            console.log(results);
+            if (err) {
+                
+                res.status(500).send({message: "Oh, sorry we could not delete that project for some strange reason"})
+            }
+            // if (results.recordset === undefined) {
+            //     res.status(400).send({message: "No project with that id exists"})
+            // }
+            res.status(201).send({message: "Project deleted successfully"})
+        })
+
+    } catch (error) {
+        res.status(500).send(error.message)
     }
 }
--- server/routes/projectRouters.js
+++ server/routes/projectRouters.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {getProjects, addProject, getSingleProject, updateProject} =require('../controllers/projects')
+const {getProjects, addProject, getSingleProject, updateProject, deleteProject} =require('../controllers/projects')
 
 router.route('/').get(getProjects);
 router.route('/:id').get(getSingleProject)
 router.route('/').post(addProject)
 router.route('/update/:id').patch(updateProject)
+router.route('/:id/delete').delete(deleteProject)
 
 module.exports = router;
