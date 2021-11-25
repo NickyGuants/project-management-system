c46bbb45b8b7af8826c035aee9655f3f587fcae1
NickyGuants <nicholasguantai528@gmail.com>
2021-11-25 13:04:52 +0300

    get single project

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -24,6 +24,29 @@ exports.getProjects = async (req, res) => {
     }
 }
 
+exports.getSingleProject = async (req, res) => {
+    try {
+        let id = parseInt(req.params.id);
+        let query = `select * from projects where project_id=${id}`;
+        let pool = await sql.connect(config);
+
+        pool.request().query(query, (err, results) => {
+            if (err) {
+                console.log(err)
+                return res.status(400).send({message: "Oh, sorry, we appear to have a server error."});
+            }
+            if (results.recordset.length === 0) {
+                return res.status(406).send("No project with that id in the database.");
+            }
+            return res.status(201).send(results.recordset[0]);
+        })
+        
+    } catch (error) {
+       res.status(500).send(error.message)
+    }
+}
+
+
 
 
 
