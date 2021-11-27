d541b66559b29f630cb0a42f0968fdb6c0228635
NickyGuants <nicholasguantai528@gmail.com>
2021-11-27 14:01:39 +0300

    create header component

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './header.css'
+import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout } from '../../redux/actions/userActions'
+
+const Header = () => {
+    const { userInfo } = useSelector(state => state.userLogin)
+    const dispatch = useDispatch();
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+
+    return (
+        <header>
+            <div className="branding"><h1>User System</h1></div>
+            <div className="user">
+                {userInfo ? (<Link to='login' onClick={logoutHandler}>Sign Out</Link>) :
+                <>    
+                <Link to='login'>Sign In</Link>
+                <Link to='register'>Sign Up</Link>
+                </>}    
+            </div>
+        </header>
+    )
+}
+
+export default Header
