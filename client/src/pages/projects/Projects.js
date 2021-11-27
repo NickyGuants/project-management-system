2355bf69381f947b07275ef1bccde717f818adbd
NickyGuants <nicholasguantai528@gmail.com>
2021-11-27 14:03:24 +0300

    projects page

---
+++ client/src/pages/projects/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './projects.css'
+
+const Projects = () => {
+    return (
+        <div>
+            <h1>Projects</h1>
+        </div>
+    )
+}
+
+export default Projects
