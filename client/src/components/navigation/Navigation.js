e6cd0426fe9c9823f34db281dcc3ec83a0fe9c88
NickyGuants <nicholasguantai528@gmail.com>
2021-11-27 14:02:38 +0300

    sidebar component

---
+++ client/src/components/navigation/Navigation.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './navigation.css'
+import {Link} from "react-router-dom"
+
+const Navigation = () => {
+    return (
+        <div className="nav">
+            <div>
+                <h1>PMS</h1>
+            </div>
+
+            <div className="nav-items">
+            <div>
+                   <Link to="/"><h2>Home</h2></Link>
+                </div>
+                <div>
+                   <Link to="/dashboard"><h2>Dashboard</h2></Link>
+                </div>
+
+                <div>
+                <Link to="/projects"><h2>Projects</h2></Link>
+                </div>
+
+                <div>
+                <Link to="/tasks"><h2>Tasks</h2></Link>
+                    
+                </div>
+
+                <div>
+                <Link to="/users"><h2>Users</h2></Link>
+                </div>
+
+                <div>
+                <Link to="/settings"><h2>Settings</h2></Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navigation
