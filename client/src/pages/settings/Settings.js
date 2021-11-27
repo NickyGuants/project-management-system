b09a8b09724d260bec45a57f702dfb775b1dfb08
NickyGuants <nicholasguantai528@gmail.com>
2021-11-27 14:03:35 +0300

    settings page

---
+++ client/src/pages/settings/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './settings.css'
+
+const Settings = () => {
+    return (
+        <div>
+            <h1>Settings</h1>
+        </div>
+    )
+}
+
+export default Settings
