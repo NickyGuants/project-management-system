adb1844dd0ffeb3a97382605d4b4de87b9c7e2ee
NickyGuants <nicholasguantai528@gmail.com>
2021-11-30 10:59:49 +0300

    restructure the code

--- api/api-logic/index.js
+++ api/api-logic/index.js
@@ -22,4 +22,3 @@ app.use('/projects', projectRoutes);
 
 
 app.listen(8001);
-
---
+++ api/background-services/config/db.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+let config = {
+    server: process.env.DB_HOST,
+    user:process.env.DB_USER,
+    database:process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: true,
+        enaableArithAbort: true,
+        trustServerCertificate: true
+    }
+
+}
+
+module.exports = config;
