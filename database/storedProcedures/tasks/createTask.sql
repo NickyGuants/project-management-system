8653160a9a827b403b07501e29487f1a8287b784
NickyGuants <nicholasguantai528@gmail.com>
2021-12-01 18:20:18 +0300

    create task

---
+++ database/storedProcedures/tasks/createTask.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE createTask(@task_name VARCHAR(200), @task_description VARCHAR(200), @is_complete BIT, @project_id INT) AS
+BEGIN
+INSERT INTO dbo.tasks (task_name, task_description, is_complete, project_id) 
+VALUES (@task_name, @task_description, @is_complete, @project_id)
+
+END
+GO
