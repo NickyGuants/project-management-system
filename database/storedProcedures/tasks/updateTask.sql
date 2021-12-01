ac9aa51e6e36d9a53cf0f13e57b5bf1ae0638490
NickyGuants <nicholasguantai528@gmail.com>
2021-12-01 18:37:05 +0300

    update task

---
+++ database/storedProcedures/tasks/updateTask.sql
@@ -0,0 +1,7 @@
+CREATE PROC updateTask (@id INT, @task_description VARCHAR(200), @task_name VARCHAR(200), @is_complete BIT) AS
+BEGIN
+Update tasks 
+set task_description=@task_description, task_name=@task_name, is_complete=@is_complete where task_id=@id
+
+END
+GO
