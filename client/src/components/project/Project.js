8eda68902cd94e31853a2a0251ea4bda36ab7b13
NickyGuants <nicholasguantai528@gmail.com>
2021-12-02 18:29:13 +0300

    create and style project card

---
+++ client/src/components/project/Project.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './project.css'
+
+const Project = () => {
+    return (
+        <div className="project-card">
+            <h1>My Project</h1>
+            <p>Project name</p>
+            <p>Complete Tasks = 13/20</p>
+            <p>Due Date: 20 Dec</p>
+        </div>
+    )
+}
+export default Project
+
