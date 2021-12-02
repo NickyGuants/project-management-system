2464a39d50c62582d397cbb8f1473e12220e75de
NickyGuants <nicholasguantai528@gmail.com>
2021-12-02 11:33:44 +0300

    assign project sp

---
+++ database/storedProcedures/users/assignProject.sql
@@ -0,0 +1,8 @@
+CREATE PROC assignProject(@project_id INT, @user_id INT) AS 
+BEGIN
+INSERT INTO project_users(user_id,project_id) VALUES(@user_id, @project_id)
+
+END
+GO
+EXEC assignProject 4, 23;
+
