df1b4023c330d120790c8ec89a9128ab3b923605
NickyGuants <nicholasguantai528@gmail.com>
2021-12-06 18:56:59 +0300

    add task reducer to root

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,12 +1,13 @@
 import { combineReducers } from "redux";
 import { userLoginReducer, userRegisterReducer } from './userReducers'
 import { getProjects } from "./projectReducers";
+import {getTasks} from "./taskReducer"
 
 const rootreducer = combineReducers({
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
-    projects: getProjects
-
+    projects: getProjects,
+    tasks: getTasks
 })
 
 export default rootreducer;
