a7f19174313e5f990da96ff10e33d1ed428b377a
NickyGuants <nicholasguantai528@gmail.com>
2021-12-06 18:56:37 +0300

    redux: task actions

---
+++ client/src/redux/actions/taskActions.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { GetTasks } from "../types";
+
+export const getProjects = () => async (dispatch) =>{
+    try {
+        dispatch({
+            type: GetTasks.Request
+        })
+
+        // const config = {
+        //     headers: {
+        //         'Content-type': 'application/json'
+        //     }
+        // }
+
+        const { data } = await axios.get('http://localhost:8000/tasks/')
+        
+        dispatch({
+            type: GetTasks.Success,
+            payload: data
+        })
+       
+    } catch (error) {
+        dispatch({
+            type: GetTasks.Fail,
+            payload: error.response && error.response.data.message ? error.response.data.message: error.message
+        })
+    }
+}
