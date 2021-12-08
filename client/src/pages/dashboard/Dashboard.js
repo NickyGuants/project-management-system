4f5edee32b07ad3213059375da1c6fd7ab77a82a
NickyGuants <nicholasguantai528@gmail.com>
2021-12-08 17:03:08 +0300

    feat: display my project on dashboard

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import './dashboard.css'
-import Project from '../../components/project/Project'
-import Task from '../../components/task/Task'
+import React from "react";
+import "./dashboard.css";
+import MyProject from "../../components/project/MyProject";
+import Task from "../../components/task/Task";
 
 const Dashboard = () => {
-    return (
-        <div className="dashboard">
-            <Project />
-            <Task />
-        </div>
-    )
-}
+  return (
+    <div className="dashboard">
+      <MyProject />
+      <Task />
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
