991ca3a6f72bf9295f33bcd10815e3e99fb3521d
NickyGuants <nicholasguantai528@gmail.com>
2021-12-08 17:01:14 +0300

    feat: get assigned project reducer

--- client/src/redux/reducers/projectReducers.js
+++ client/src/redux/reducers/projectReducers.js
@@ -1,21 +1,41 @@
-import { GetProjects } from '../types'
+import { GetAssignedProject, GetProjects } from "../types";
 
 export const getProjects = (state = {}, action) => {
-    switch (action.type) {
-        case GetProjects.Request:
-            return { loading: true }
-        
-        case GetProjects.Success:
-            return {
-                loading: false,
-                projects: action.payload
-            }
-        case GetProjects.Fail:
-            return {
-                loading: false,
-                error: action.payload
-            }
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case GetProjects.Request:
+      return { loading: true };
+
+    case GetProjects.Success:
+      return {
+        loading: false,
+        projects: action.payload,
+      };
+    case GetProjects.Fail:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const getAssignedProject = (state = {}, action) => {
+  switch (action.type) {
+    case GetAssignedProject.Request:
+      return { loading: true };
+
+    case GetAssignedProject.Success:
+      return {
+        loading: false,
+        project: action.payload,
+      };
+    case GetAssignedProject.Fail:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
