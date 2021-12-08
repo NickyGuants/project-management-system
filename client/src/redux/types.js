8d42e63dc290480c06eded0e7227661b0ebabf38
NickyGuants <nicholasguantai528@gmail.com>
2021-12-08 17:02:01 +0300

    assigned project type

--- client/src/redux/types.js
+++ client/src/redux/types.js
@@ -1,28 +1,33 @@
 export const Register = {
-    Request: "REGISTER_REQUEST",
-    Success: "REGISTER_SUCCESS",
-    Fail: "REGISTER_FAIL"
-}
+  Request: "REGISTER_REQUEST",
+  Success: "REGISTER_SUCCESS",
+  Fail: "REGISTER_FAIL",
+};
 export const Login = {
-    Request: "LOGIN_REQUEST",
-    Success: "LOGIN_SUCCESS",
-    Fail: "LOGIN_FAIL"
-}
-export const USER_LOGOUT = "USER_LOGOUT"
+  Request: "LOGIN_REQUEST",
+  Success: "LOGIN_SUCCESS",
+  Fail: "LOGIN_FAIL",
+};
+export const USER_LOGOUT = "USER_LOGOUT";
 
 export const GetUsers = {
-    Request: "User_Request",
-    Success: "User_Success",
-    Fail: "User_Fail"
-}
+  Request: "User_Request",
+  Success: "User_Success",
+  Fail: "User_Fail",
+};
 
 export const GetProjects = {
-    Request: "Project_Request",
-    Success: "Project_Success",
-    Fail: "Project_Fail"
-}
+  Request: "Project_Request",
+  Success: "Project_Success",
+  Fail: "Project_Fail",
+};
+export const GetAssignedProject = {
+  Request: "Project_Request",
+  Success: "Project_Success",
+  Fail: "Project_Fail",
+};
 export const GetTasks = {
-    Request: "Task_Request",
-    Success: "Task_Success",
-    Fail: "Task_Fail"
-}
+  Request: "Task_Request",
+  Success: "Task_Success",
+  Fail: "Task_Fail",
+};
