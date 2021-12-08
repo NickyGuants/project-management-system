f752b4c2f63f165586c9000a6daaa29a22d627cb
NickyGuants <nicholasguantai528@gmail.com>
2021-12-08 17:02:31 +0300

    display assigne project

--- client/src/pages/users/Users.js
+++ client/src/pages/users/Users.js
@@ -1,56 +1,63 @@
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { getUsers } from '../../redux/actions/userActions'
-import './users.css'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUsers } from "../../redux/actions/userActions";
+import "./users.css";
+import AssignProject from "../../components/project/AssignProject";
 
 const Users = () => {
-    const { users } = useSelector(state => state.users)
-    const userLogin = useSelector(state => state.userLogin)
+  const { users } = useSelector((state) => state.users);
+  const userLogin = useSelector((state) => state.userLogin);
 
-    const { userInfo } = userLogin;
-    let token = userInfo?.token
+  const { userInfo } = userLogin;
+  let token = userInfo?.token;
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getUsers(token))
-    }, [dispatch,token])
+  useEffect(() => {
+    dispatch(getUsers(token));
+  }, [dispatch, token]);
 
+  return (
+    <div className="users">
+      <div className="user-table">
+        <table>
+          <tbody className="body">
+            <tr>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Project</th>
+              <th>Assign Project</th>
+              <th>Assign Task</th>
+              <th>Delete User</th>
+            </tr>
+            {users?.map((user) => {
+              return (
+                <>
+                  <tr key={user.id} className="user-row">
+                    <td>{user.name}</td>
+                    <td>{user.username}</td>
+                    <td>{user.email}</td>
+                    <td>Project Name</td>
+                    <td>
+                      {/* <button onClick={handleClickOpen}>Assign Project</button> */}
+                      <AssignProject />
+                    </td>
+                    <td>
+                      <button>Assign Task</button>
+                    </td>
+                    <td>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="users">
-            <div className="user-table">
-                <table>
-                    <tbody className="body">
-                        <tr>
-                            <th>Name</th>
-                            <th>Username</th>
-                            <th>Email</th>
-                            <th>Project</th>
-                            <th>Assign Project</th>
-                            <th>Assign Task</th>
-                            <th>Delete User</th>
-                        </tr>
-                        {users?.map((user) => {
-                            return (
-                            <>
-                                <tr key={user.id} className="user-row">
-                                        <td>{user.name}</td>
-                                        <td>{ user.username}</td>
-                                        <td>{user.email}</td>
-                                        <td>Project Name</td>
-                                        <td><button >Assign Project</button></td>
-                                    <td><button >Assign Task</button></td>
-                                    <td><button >Delete</button></td>
-                                </tr>
-                                    
-                            </>   
-                        )})}
-                        </tbody>
-                        </table>
-                    </div>
-        </div>
-    )
-}
-
-export default Users
+export default Users;
