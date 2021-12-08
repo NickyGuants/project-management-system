f5ce7eab5a46ea8918bdd07342664f526fd90522
NickyGuants <nicholasguantai528@gmail.com>
2021-12-08 17:01:48 +0300

    feat: assigned project

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,14 +1,19 @@
 import { combineReducers } from "redux";
-import { getUsers, userLoginReducer, userRegisterReducer } from './userReducers'
-import { getProjects } from "./projectReducers";
-import {getTasks} from "./taskReducer"
+import {
+  getUsers,
+  userLoginReducer,
+  userRegisterReducer,
+} from "./userReducers";
+import { getAssignedProject, getProjects } from "./projectReducers";
+import { getTasks } from "./taskReducer";
 
 const rootreducer = combineReducers({
-    userLogin: userLoginReducer,
-    userRegister: userRegisterReducer,
-    projects: getProjects,
-    tasks: getTasks,
-    users: getUsers
-})
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  projects: getProjects,
+  tasks: getTasks,
+  users: getUsers,
+  assignedProject: getAssignedProject,
+});
 
 export default rootreducer;
