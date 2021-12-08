a76fe493b51c827f2ef513d018b6316174bd01e3
NickyGuants <nicholasguantai528@gmail.com>
2021-12-08 17:01:29 +0300

    feat: get assigned project action

--- client/src/redux/actions/projectActions.js
+++ client/src/redux/actions/projectActions.js
@@ -1,29 +1,64 @@
 import axios from "axios";
-import { GetProjects } from "../types";
+import { GetAssignedProject, GetProjects } from "../types";
 
-export const getProjects = () => async (dispatch) =>{
-    try {
-        dispatch({
-            type: GetProjects.Request
-        })
+export const getProjects = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: GetProjects.Request,
+    });
 
-        // const config = {
-        //     headers: {
-        //         'Content-type': 'application/json'
-        //     }
-        // }
+    // const config = {
+    //     headers: {
+    //         'Content-type': 'application/json'
+    //     }
+    // }
 
-        const { data } = await axios.get('http://localhost:8000/projects/')
-        
-        dispatch({
-            type: GetProjects.Success,
-            payload: data
-        })
-       
-    } catch (error) {
-        dispatch({
-            type: GetProjects.Fail,
-            payload: error.response && error.response.data.message ? error.response.data.message: error.message
-        })
-    }
-}
+    const { data } = await axios.get("http://localhost:8000/projects/");
+
+    dispatch({
+      type: GetProjects.Success,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GetProjects.Fail,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const getAssignedProject = (user_id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: GetAssignedProject.Request,
+    });
+
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+
+    const { data } = await axios.get(
+      `http://localhost:8000/projects/assigned/${user_id}`,
+      // { params: { user_id: user_id } },
+      config
+    );
+
+    dispatch({
+      type: GetAssignedProject.Success,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GetAssignedProject.Fail,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
