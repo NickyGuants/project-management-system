f1591e310ab0b5a6e3c6be6d911e0100fd23afd1
NickyGuants <nicholasguantai528@gmail.com>
2021-12-08 14:29:18 +0300

    assign project routes

--- api/api-logic/routes/projectRouters.js
+++ api/api-logic/routes/projectRouters.js
@@ -1,11 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {getProjects, addProject, getSingleProject, updateProject, deleteProject} =require('../controllers/projects')
+const {
+  getProjects,
+  addProject,
+  getSingleProject,
+  updateProject,
+  deleteProject,
+  assignProject,
+  getAssignedProject,
+} = require("../controllers/projects");
 
-router.route('/').get(getProjects);
-router.route('/:id').get(getSingleProject)
-router.route('/').post(addProject)
-router.route('/update/:id').put(updateProject)
-router.route('/:id/delete').delete(deleteProject)
+router.route("/").get(getProjects);
+router.route("/assigned").get(getAssignedProject);
+router.route("/:id").get(getSingleProject);
+router.route("/").post(addProject);
+router.route("/update/:id").put(updateProject);
+router.route("/:id/delete").delete(deleteProject);
+router.route("/assign").post(assignProject);
 
 module.exports = router;
