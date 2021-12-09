95d66f21c37b541cd91394f340ca57e95d10186b
NickyGuants <nicholasguantai528@gmail.com>
2021-12-09 16:01:16 +0300

    create admin panel

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -1,22 +1,27 @@
 import React from "react";
+import "./admin.css";
 import { Link, Outlet } from "react-router-dom";
+import Navigation from "../navigation/Navigation";
 
 const Admin = () => {
   return (
-    <div>
-      <h1>Admin panel</h1>
-      <nav>
-        <Link to="/admin/projects">
-          <h2>Projects</h2>
-        </Link>
-        <Link to="/admin/tasks">
-          <h2>Tasks</h2>
-        </Link>
-        <Link to="/admin/users">
-          <h2>Users</h2>
-        </Link>
-      </nav>
-      <Outlet />
+    <div className="container">
+      <Navigation />
+      <div className="main">
+        <nav>
+          <h1>Admin Panel</h1>
+          <Link to="/admin/projects">
+            <h2>Projects</h2>
+          </Link>
+          <Link to="/admin/tasks">
+            <h2>Tasks</h2>
+          </Link>
+          <Link to="/admin/users">
+            <h2>Users</h2>
+          </Link>
+        </nav>
+        <Outlet />
+      </div>
     </div>
   );
 };
