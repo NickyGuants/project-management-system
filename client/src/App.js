c705882b5c4b3c07d9b65d5106696e952a78bd05
NickyGuants <nicholasguantai528@gmail.com>
2021-12-09 16:01:01 +0300

    restructure app

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,34 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/header/Header";
 import Register from "./components/register/Register";
 import Login from "./components/login/Login";
-import Navigation from "./components/navigation/Navigation";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Projects from "./pages/projects/Projects";
 import Tasks from "./pages/tasks/Tasks";
 import Users from "./pages/users/Users";
 import Settings from "./pages/settings/Settings";
 import Admin from "./components/admin/Admin";
+import LandingPage from "./pages/landing/LandingPage";
 
 function App() {
   return (
     <div className="app">
       <Router>
-        <div className="container">
-          <Navigation />
-          <div className="main">
-            <Header />
-            <Routes>
-              <Route path="/dashboard" element={<Dashboard />} />
+        <Routes>
+          <Route path="/" element={<LandingPage />}>
+            <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+          </Route>
+          <Route path="/dashboard" element={<Dashboard />} />
 
-              <Route path="/admin" element={<Admin />}>
-                <Route path="projects" element={<Projects />} />
-                <Route path="tasks" element={<Tasks />} />
-                <Route path="users" element={<Users />} />
-              </Route>
+          <Route path="/admin" element={<Admin />}>
+            <Route path="projects" element={<Projects />} />
+            <Route path="tasks" element={<Tasks />} />
+            <Route path="users" element={<Users />} />
+          </Route>
 
-              <Route path="/settings" element={<Settings />} />
-
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </div>
-        </div>
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
       </Router>
     </div>
   );
