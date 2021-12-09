e3d9de0c59e0c7848e9471ed2cd7e19a38ba4967
NickyGuants <nicholasguantai528@gmail.com>
2021-12-09 16:02:07 +0300

    restructure code

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -2,12 +2,20 @@ import React from "react";
 import "./dashboard.css";
 import MyProject from "../../components/project/MyProject";
 import Task from "../../components/task/Task";
+import Navigation from "../../components/navigation/Navigation";
+import Header from "../../components/header/Header";
 
 const Dashboard = () => {
   return (
-    <div className="dashboard">
-      <MyProject />
-      <Task />
+    <div className="container">
+      <Navigation />
+      <div className="dashboard">
+        <Header />
+        <div className="main">
+          <MyProject />
+          <Task />
+        </div>
+      </div>
     </div>
   );
 };
