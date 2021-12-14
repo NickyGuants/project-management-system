db8bfc6a13c7aea7cb55f8e468917936374a3c6f
NickyGuants <nicholasguantai528@gmail.com>
2021-12-14 21:42:21 +0300

    refactor

--- api/api-logic/index.js
+++ api/api-logic/index.js
@@ -1,23 +1,25 @@
-const sql = require('mssql');
-const express = require('express');
-const config = require('./config/db');
-const projectRoutes = require('./routes/projectRouters');
-const taskRoutes = require('./routes/taskRoutes')
-const cors = require('cors')
+const sql = require("mssql");
+const express = require("express");
+const config = require("./config/db");
+const projectRoutes = require("./routes/projectRouters");
+const taskRoutes = require("./routes/taskRoutes");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-sql.connect(config).then(pool => {
+sql
+  .connect(config)
+  .then((pool) => {
     if (pool.connected) {
-        console.log("connected to the msssql database");
+      console.log("connected to the msssql database");
     }
-}).catch(e => console.log(e));
+  })
+  .catch((e) => console.log(e));
 
-app.use('/projects', projectRoutes);
-app.use('/tasks', taskRoutes)
-
+app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
 
 app.listen(8000);
