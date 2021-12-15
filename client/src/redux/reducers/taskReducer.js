2f97a1fb5a6cd432425aba1c38c2733f4de4d932
NickyGuants <nicholasguantai528@gmail.com>
2021-12-15 11:49:21 +0300

    create add task reducer

--- client/src/redux/reducers/taskReducer.js
+++ client/src/redux/reducers/taskReducer.js
@@ -1,21 +1,41 @@
-import { GetTasks } from '../types'
+import { AddTask, GetTasks } from "../types";
 
 export const getTasks = (state = {}, action) => {
-    switch (action.type) {
-        case GetTasks.Request:
-            return { loading: true }
-        
-        case GetTasks.Success:
-            return {
-                loading: false,
-                tasks: action.payload
-            }
-        case GetTasks.Fail:
-            return {
-                loading: false,
-                error: action.payload
-            }
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case GetTasks.Request:
+      return { loading: true };
+
+    case GetTasks.Success:
+      return {
+        loading: false,
+        tasks: action.payload,
+      };
+    case GetTasks.Fail:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+export const addTask = (state = {}, action) => {
+  switch (action.type) {
+    case AddTask.Request:
+      return {
+        loading: true,
+      };
+    case AddTask.Success:
+      return {
+        loading: false,
+        taskInfo: action.payload,
+      };
+    case AddTask.Fail:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
