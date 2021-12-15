9c68d9ca16cdec2a21c15d69db052965f16bff4f
NickyGuants <nicholasguantai528@gmail.com>
2021-12-15 11:48:06 +0300

    create task

--- client/src/pages/admin/tasks/CreateTaskModal.js
+++ client/src/pages/admin/tasks/CreateTaskModal.js
@@ -1,13 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProjects } from "../../../redux/actions/projectActions";
+import { addTask } from "../../../redux/actions/taskActions";
 import "./modal.css";
 
 const CreateTaskModal = ({ show, close }) => {
+  const [task_name, setTaskName] = useState("");
+  const [task_description, setDescription] = useState("");
+  const [due_date, setDueDate] = useState("");
+  const [project_id, setProjectId] = useState("");
+
   const dispatch = useDispatch();
 
   const AllProjects = useSelector((state) => state.projects);
   const { projects } = AllProjects;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addTask(task_name, task_description, due_date, project_id));
+    close();
+  };
 
   useEffect(() => {
     dispatch(getProjects());
@@ -22,10 +34,17 @@ const CreateTaskModal = ({ show, close }) => {
               <h2> Create A New Task </h2>
             </header>
             <main className="modal_content">
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="select">
                   <label htmlFor="projects">Choose a Project</label>
-                  <select name="projects" id="projects">
+                  <select
+                    name="projects"
+                    id="projects"
+                    onChange={(e) => setProjectId(e.target.value)}
+                  >
+                    <option value="" selected>
+                      Choose Project
+                    </option>
                     {projects?.map((project) => (
                       <option value={project.project_id}>
                         {project.project_name}
@@ -35,32 +54,45 @@ const CreateTaskModal = ({ show, close }) => {
                 </div>
                 <div className="task-name">
                   <label htmlFor="task_name">Task Name</label>
-                  <input type="text" placeholder="Enter name of the Task" />
+                  <input
+                    type="text"
+                    placeholder="Enter name of the Task"
+                    value={task_name}
+                    onChange={(e) => setTaskName(e.target.value)}
+                  />
                 </div>
 
                 <div className="task-description">
                   <label htmlFor="task+description">Task Description</label>
                   <textarea
-                    name=""
+                    name="task_description"
+                    value={task_description}
                     id=""
                     cols="30"
                     rows="10"
                     placeholder="Enter task description"
+                    onChange={(e) => setDescription(e.target.value)}
                   ></textarea>
                 </div>
                 <div className="due-date">
                   <label htmlFor="due_date">Due Date</label>
-                  <input type="Date" />
+                  <input
+                    type="Date"
+                    value={due_date}
+                    onChange={(e) => setDueDate(e.target.value)}
+                  />
                 </div>
+                <footer className="modal_footer">
+                  <button className="modal-close" onClick={() => close()}>
+                    Cancel
+                  </button>
+
+                  <button className="submit" type="submit">
+                    Submit
+                  </button>
+                </footer>
               </form>
             </main>
-            <footer className="modal_footer">
-              <button className="modal-close" onClick={() => close()}>
-                Cancel
-              </button>
-
-              <button className="submit">Submit</button>
-            </footer>
           </div>
         </div>
       ) : null}
