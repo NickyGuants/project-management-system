2159bbe0899f6186a0ef34211187cfda8202a3a3
NickyGuants <nicholasguantai528@gmail.com>
2021-12-15 11:48:49 +0300

    add task action

--- client/src/redux/actions/taskActions.js
+++ client/src/redux/actions/taskActions.js
@@ -1,29 +1,65 @@
 import axios from "axios";
-import { GetTasks } from "../types";
+import { AddTask, GetTasks } from "../types";
 
-export const getTasks = () => async (dispatch) =>{
+export const getTasks = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: GetTasks.Request,
+    });
+
+    // const config = {
+    //     headers: {
+    //         'Content-type': 'application/json'
+    //     }
+    // }
+
+    const { data } = await axios.get("http://localhost:8000/tasks/");
+
+    dispatch({
+      type: GetTasks.Success,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GetTasks.Fail,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const addTask =
+  (task_name, task_description, due_date, project_id) => async (dispatch) => {
     try {
-        dispatch({
-            type: GetTasks.Request
-        })
+      dispatch({
+        type: AddTask.Request,
+      });
 
-        // const config = {
-        //     headers: {
-        //         'Content-type': 'application/json'
-        //     }
-        // }
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
 
-        const { data } = await axios.get('http://localhost:8000/tasks/')
-        
-        dispatch({
-            type: GetTasks.Success,
-            payload: data
-        })
-       
+      const { data } = await axios.post(
+        "http://localhost:8000/tasks/",
+        { task_name, task_description, due_date, project_id },
+        config
+      );
+
+      dispatch({
+        type: AddTask.Success,
+        payload: data,
+      });
     } catch (error) {
-        dispatch({
-            type: GetTasks.Fail,
-            payload: error.response && error.response.data.message ? error.response.data.message: error.message
-        })
+      dispatch({
+        type: AddTask.Fail,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
     }
-}
+  };
