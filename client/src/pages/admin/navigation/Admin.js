dba42f53d351800ff733bb897a6f6eaa0df0cc5e
NickyGuants <nicholasguantai528@gmail.com>
2021-12-16 09:27:00 +0300

    add icons and some styling

--- client/src/pages/admin/navigation/Admin.js
+++ client/src/pages/admin/navigation/Admin.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./navigation.css";
 import { Link, Outlet } from "react-router-dom";
 import Header from "../../../components/header/Header";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import TaskIcon from "@mui/icons-material/Task";
+import PeopleIcon from "@mui/icons-material/People";
+import AssignmentIcon from "@mui/icons-material/Assignment";
 
 const Admin = () => {
   return (
@@ -10,23 +14,27 @@ const Admin = () => {
         <div className="nav-items">
           <div className="nav-item">
             <Link to="/admin/dashboard">
+              <DashboardIcon />
               <h2>Dashboard</h2>
             </Link>
           </div>
 
           <div className="nav-item">
             <Link to="/admin/projects">
+              <AssignmentIcon />
               <h2>Projects</h2>
             </Link>
           </div>
 
           <div className="nav-item">
             <Link to="/admin/tasks">
+              <TaskIcon />
               <h2>Tasks</h2>
             </Link>
           </div>
           <div className="nav-item">
             <Link to="/admin/users">
+              <PeopleIcon />
               <h2>Users</h2>
             </Link>
           </div>
